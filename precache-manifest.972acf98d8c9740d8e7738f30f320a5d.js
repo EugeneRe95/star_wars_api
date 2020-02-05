d496987eec4803e3078a6d68867794387462240f
Eugene Revutskiy <revytskiy.evgen@gmail.com>
2020-02-05 22:13:04 +0200

    Updates

---
+++ precache-manifest.972acf98d8c9740d8e7738f30f320a5d.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d9d3b2743c2e3f9ad46c35b8efbb8a5",
+    "url": "/star_wars_api/index.html"
+  },
+  {
+    "revision": "b959d0af995174d25061",
+    "url": "/star_wars_api/static/css/2.1c3d3005.chunk.css"
+  },
+  {
+    "revision": "d6b9f4a79d356509597f",
+    "url": "/star_wars_api/static/css/main.06a12020.chunk.css"
+  },
+  {
+    "revision": "b959d0af995174d25061",
+    "url": "/star_wars_api/static/js/2.83c47901.chunk.js"
+  },
+  {
+    "revision": "3adc01bea76e7956dc3633ee898f6936",
+    "url": "/star_wars_api/static/js/2.83c47901.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d6b9f4a79d356509597f",
+    "url": "/star_wars_api/static/js/main.1805a580.chunk.js"
+  },
+  {
+    "revision": "30422eb34a6d494a5e1c",
+    "url": "/star_wars_api/static/js/runtime-main.a2d058ae.js"
+  },
+  {
+    "revision": "ac3fa3db0aa1785ff53bdfc1dafb0121",
+    "url": "/star_wars_api/static/media/bb8.ac3fa3db.png"
+  },
+  {
+    "revision": "d78ee0df504d7a86ca4f114d1301e294",
+    "url": "/star_wars_api/static/media/eagle.d78ee0df.png"
+  },
+  {
+    "revision": "067595ad77ecc0db9c81c8905a7eef32",
+    "url": "/star_wars_api/static/media/fa-brands-400.067595ad.woff2"
+  },
+  {
+    "revision": "57dcda6f368ea90179f75cbdae96c263",
+    "url": "/star_wars_api/static/media/fa-brands-400.57dcda6f.eot"
+  },
+  {
+    "revision": "9d67fa1429375bd2a899a17eb77d0342",
+    "url": "/star_wars_api/static/media/fa-brands-400.9d67fa14.svg"
+  },
+  {
+    "revision": "9ec698d1a597bff5df337094b71ddaaf",
+    "url": "/star_wars_api/static/media/fa-brands-400.9ec698d1.ttf"
+  },
+  {
+    "revision": "b564da88bbf0c4aa446fa19653713cd1",
+    "url": "/star_wars_api/static/media/fa-brands-400.b564da88.woff"
+  },
+  {
+    "revision": "3351f435b3c9037fd88aeb04dc1e43bc",
+    "url": "/star_wars_api/static/media/fa-regular-400.3351f435.eot"
+  },
+  {
+    "revision": "4165c2688309cbfb1b877caf8f75afb5",
+    "url": "/star_wars_api/static/media/fa-regular-400.4165c268.woff2"
+  },
+  {
+    "revision": "5d0861781aeef6c82fda3a3076954a1b",
+    "url": "/star_wars_api/static/media/fa-regular-400.5d086178.svg"
+  },
+  {
+    "revision": "73cf49a2232c06c920b7a34e36bfb58c",
+    "url": "/star_wars_api/static/media/fa-regular-400.73cf49a2.woff"
+  },
+  {
+    "revision": "a0e3ac82940c1998c5977fd4bc1f5ef6",
+    "url": "/star_wars_api/static/media/fa-regular-400.a0e3ac82.ttf"
+  },
+  {
+    "revision": "0724bb8b89ab6b8b9b7df917b17be0b7",
+    "url": "/star_wars_api/static/media/fa-solid-900.0724bb8b.svg"
+  },
+  {
+    "revision": "55eb2a60e8181f0e68b558c991973bf0",
+    "url": "/star_wars_api/static/media/fa-solid-900.55eb2a60.woff2"
+  },
+  {
+    "revision": "75f38a159982b6bd1704891332d95fa7",
+    "url": "/star_wars_api/static/media/fa-solid-900.75f38a15.ttf"
+  },
+  {
+    "revision": "89e02bae13c9131c7468b1e729339ac1",
+    "url": "/star_wars_api/static/media/fa-solid-900.89e02bae.eot"
+  },
+  {
+    "revision": "cdfec5cf5e9840889790bcf2c4042583",
+    "url": "/star_wars_api/static/media/fa-solid-900.cdfec5cf.woff"
+  }
+]);
